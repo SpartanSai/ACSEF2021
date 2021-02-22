a73aae8c1b822f06e1497b9a0243fe71985d1194
Sai Divyesh Tunguturu <spartansai05@gmail.com>
2021-02-21 22:34:01 -0800

    Download and cars_in_pictures

--- Download.py
+++ Download.py
@@ -3,28 +3,40 @@ import requests # to get image from the web
 import shutil # to save it locally
 import time # to sleep the thread
 import os
+from subprocess import call
 
-for x in range(138, 150):
-    time.sleep(65.0)
+for x in range(70, 150):
     # Set up the image URL and filename
-    image_url = "http://www.durham.gov.uk/article/23827/A690-Crossgate-Peth"
+    image_url = "http://www.durham.gov.uk/article/6140/Gilesgate"
     filename = image_url.split("/")[-1]
     filename = filename.split(".")[0]+"_{}.jpg".format(x)
 
     # Open the url image, set stream to True, this will return the stream content.
     r = requests.get(image_url, stream = True)
-    path = os.path.join("C:/Users/varun/Python/ACSEF_2021/Images/{}".format(filename))
+    stringpath = "/Users/saimonish/IntelliJ_workspace/ACSEF2021/Images/{}".format(filename)
+    #stringpath = "C:/Users/varun/Python/ACSEF_2021/Images/{}".format(filename)
+
+    path = os.path.join(stringpath)
+    
 
     # Check if the image was retrieved successfully
     if r.status_code == 200:
         # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
 
         r.raw.decode_content = True
-
+    
         # Open a local file with wb ( write binary ) permission.
         with open(path,'wb') as f:
             shutil.copyfileobj(r.raw, f)
-
+        
         print('Image sucessfully Downloaded: ',filename)
     else:
         print('Image Couldn\'t be retreived')
+
+    
+    call(["open", stringpath])
+    
+    userinput = input("How many cars are in the picture?")
+    print(userinput)
+    
+    time.sleep(65.0)
