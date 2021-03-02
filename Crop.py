5aa96ebd80d7cdc90a563db9d93e3cca2fc34086
DESKTOP-F9PJSQ7\varun <varrao23@gmail.com>
2021-03-02 09:53:27 -0800

    images 183 - 249

--- Crop.py
+++ Crop.py
@@ -2,21 +2,22 @@ from PIL import Image, ImageOps
 import numpy as np
 
 
-for number in range(1,150):
-    if number!=69 and number!=117:
-        image = Image.open(r"/Users/saimonish/IntelliJ_workspace/ACSEF2021/Images/dutmc_09_{}.jpg".format(number)) 
+for number in range(183,250):
+    #image = Image.open(r"/Users/saimonish/IntelliJ_workspace/ACSEF2021/Images/dutmc_09_{}.jpg".format(number)) 
+    image = Image.open(r"C:/Users/varun/Python/ACSEF_2021/Images/dutmc_09_{}.jpg".format(number)) 
 
-        #target will be 870x484 pixels
-        left = 734
-        upper = 1080-484
-        right = 1586
-        lower = 1080
+    #target will be 870x484 pixels
+    left = 734
+    upper = 1080-484
+    right = 1586
+    lower = 1080
 
-        image1 = image.crop((left,upper,right,lower))
-        #image1 = ImageOps.grayscale(image1)
-        image1 = image1.convert("LA")
-        image1.save('/Users/saimonish/IntelliJ_workspace/ACSEF2021/CroppedImages/dutmc_09_{}_cropped.png'.format(number), "PNG")
-        print("Save Successful!")
+    image1 = image.crop((left,upper,right,lower))
+    #image1 = ImageOps.grayscale(image1)
+    image1 = image1.convert("LA")
+    # image1.save('/Users/saimonish/IntelliJ_workspace/ACSEF2021/CroppedImages/dutmc_09_{}_cropped.png'.format(number), "PNG")
+    image1.save("C:/Users/varun/Python/ACSEF_2021/CroppedImages/dutmc_09_{}_cropped.png".format(number), "PNG")
+    print("Save Successful!")
         
 '''
 image = Image.open("/Users/saimonish/IntelliJ_workspace/ACSEF2021/CroppedImages/dutmc_09_1_cropped.png")
