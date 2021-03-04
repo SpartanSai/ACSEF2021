83df41e5216405f2bc1c981e9152319106189b05
DESKTOP-F9PJSQ7\varun <varrao23@gmail.com>
2021-03-04 00:35:02 -0800

    data analysis part one

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -5,9 +5,6 @@ Created on Wed Mar  3 20:59:08 2021
 @author: varun, sai divyesh
 """
 
-file = open("C:/Users/varun/Python/ACSEF_2021/cars_in_pictures.txt", "r")
-#file = open("/Users/saimonish/IntelliJ_workspace/ACSEF2021/cars_in_pictures.txt", "r")
-
 #       |   *   |
 #       |   *   |
 #       |0 1*   |
@@ -25,12 +22,16 @@ file = open("C:/Users/varun/Python/ACSEF_2021/cars_in_pictures.txt", "r")
 #       |   *   |
 #       |   *   |
 
+file = open("C:/Users/varun/Python/ACSEF_2021/cars_in_pictures.txt", "r")
+#file = open("/Users/saimonish/IntelliJ_workspace/ACSEF2021/cars_in_pictures.txt", "r")
+f = open("C:/Users/varun/Python/ACSEF_2021/waiting_times.txt", "w")
+#f = open("/Users/saimonish/IntelliJ_workspace/ACSEF2021/waiting_times.txt", "w")
 sums = [-1]*8
 print('       |   *   |\n       |   *   |\n       |0 1*   |\n       |   *   |\n_______|F L*   |_______\n                F 2\n                L 3\n* * * *         * * * * \n    7 L\n    6 F\n_______         _______\n       |   *L F|\n       |   *   |\n       |   *5 4|\n       |   *   |\n       |   *   |')
 for i in range(0, 400, 8):
     A = []
     for j in range(0, 8):
-        A.append(int(file.readline()))
+        A.append(int(file.readline()))    
     sums[0] = A[0] + A[1]
     sums[1] = A[2] + A[3]
     sums[2] = A[4] + A[5]
@@ -47,7 +48,6 @@ for i in range(0, 400, 8):
             if m < sums[k]:
                 ind = k
                 m = sums[k]
-        sums[ind] = -1
         if ind == 0:
             print('The lights are green for the cars in lanes 0 and 1 (using the diagram above)')
         elif ind == 1:
@@ -64,4 +64,8 @@ for i in range(0, 400, 8):
             print('The lights are green for the cars in lanes 2 and 6 (using the diagram above)')
         elif ind == 7:
             print('The lights are green for the cars in lanes 3 and 7 (using the diagram above)')
-            
+        
+        for k in range(0, sums[ind]):
+            f.write(str(j) + '/n')
+        sums[ind] = -1
+    f.write('==== /n')
